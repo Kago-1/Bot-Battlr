477a52814313ea73240205ad70c3803c9b9b56ae
Steve <stephenkago10@gmail.com>
2022-10-02 01:15:51 +0300

    render a list of bots

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,11 +1,16 @@
 import React from "react";
+import BotSpecs from "./BotSpecs";
 
-function BotCollection() {
+function BotCollection({bots}) {
   // Your code here
   return (
     <div className="ui four column grid">
       <div className="row">
         {/*...and here..*/}
+        {bots.map((bot)=> <BotSpecs
+        key={bot.id}
+        bot={bot} />
+)}
         Collection of all bots
       </div>
     </div>
